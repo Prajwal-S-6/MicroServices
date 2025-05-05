6d2ef8df8e4637b566599a7acfd702e0259b23bb
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-05-05 08:08:36 +0530

    grpc server -billingService

---
+++ patient-management/billing-service/src/main/java/com/pm/billing_service/billing_service/grpc/BillingServiceGRPCServer.java
@@ -0,0 +1,25 @@
+package com.pm.billing_service.billing_service.grpc;
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import io.grpc.stub.StreamObserver;
+
+@GrpcService
+public class BillingServiceGRPCServer extends BillingServiceGrpc.BillingServiceImplBase {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BillingServiceGRPCServer.class);
+
+    @Override
+    public void createBillingAccount(BillingRequest request, StreamObserver<BillingResponse> billingResponse) {
+        LOG.info("createBillingAccount request received {} from grpc client", request.toString());
+
+        BillingResponse response = BillingResponse.newBuilder().setAccountId("123").setStatus("ACTIVE").build();
+
+        billingResponse.onNext(response);
+        billingResponse.onCompleted();
+    }
+}
