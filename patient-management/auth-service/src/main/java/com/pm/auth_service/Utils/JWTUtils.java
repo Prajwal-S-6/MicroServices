c1e3a8865f36b9ad2bd3d6d53b90214955fe7950
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-05-08 00:28:34 +0530

    jwt token validation

--- patient-management/auth-service/src/main/java/com/pm/auth_service/Utils/JWTUtils.java
+++ patient-management/auth-service/src/main/java/com/pm/auth_service/Utils/JWTUtils.java
@@ -1,19 +1,25 @@
 package com.pm.auth_service.Utils;
 
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.security.SignatureException;
 import java.util.Base64;
 import java.util.Date;
 
 @Component
 public class JWTUtils {
 
+    private static final Logger LOG = LoggerFactory.getLogger(JWTUtils.class);
     private final Key secretKey;
 
     public JWTUtils(@Value("${jwt.secret}") String secretKey) {
@@ -28,4 +34,17 @@ public class JWTUtils {
                 .signWith(secretKey)
                 .compact();
     }
+
+    public void validateToken(String token) {
+        try {
+            LOG.info(token);
+            Jwts.parser().verifyWith((SecretKey) secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+        } catch (JwtException e) {
+            throw new JwtException("Invalid JWT");
+        }
+    }
+
+
 }
--- patient-management/auth-service/src/main/java/com/pm/auth_service/controller/AuthController.java
+++ patient-management/auth-service/src/main/java/com/pm/auth_service/controller/AuthController.java
@@ -5,6 +5,8 @@ import com.pm.auth_service.dto.UserLoginResponseDTO;
 import com.pm.auth_service.service.AuthService;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,7 @@ import java.util.Optional;
 @RequestMapping(value = "user")
 public class AuthController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(AuthController.class);
     private final AuthService authService;
 
     public AuthController(AuthService authService) {
@@ -27,9 +30,21 @@ public class AuthController {
             @ApiResponse(responseCode = "401", description = "UnAuthorized")})
     public ResponseEntity<UserLoginResponseDTO> login(@RequestBody UserLoginRequestDTO loginRequestDTO) {
         Optional<String> authToken =  authService.authenticate(loginRequestDTO);
-        if(authToken.isPresent()) {
+        if(authToken.isEmpty()) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
         return ResponseEntity.ok().body(new UserLoginResponseDTO(authToken.get()));
+    }
+
+
+    @GetMapping(value = "/validate")
+    public ResponseEntity<Void> validateToken(@RequestHeader("Authorization") String authHeader) {
+        // In the header--------- Authorization: Bearer <token>
+        if(authHeader == null || !authHeader.startsWith("Bearer")) {
+            LOG.info("Invalid Header");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return authService.isValidToken(authHeader.substring(7)) ? ResponseEntity.ok().build() :
+                ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
