36cc25989558cacaa230b89bc9a79191a5d46a7c
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-05-12 18:39:23 +0530

    creating vpc

--- patient-management/Infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ patient-management/Infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,12 +1,25 @@
 package com.pm.stack;
 
 
+
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.Vpc;
 
 public class LocalStack extends Stack {
 
+    private final Vpc vpc;
     public LocalStack(final App scope, final String id, final StackProps props) {
         super(scope, id, props);
+
+        this.vpc = createVpc();
+    }
+
+    private Vpc createVpc() {
+        return Vpc.Builder.create(this, "PatientManagementVPC")
+                .vpcName("PatientManagementVPC")
+                .maxAzs(2)
+                .build();
+
     }
 
     public static void main(String[] args) {
@@ -17,4 +30,6 @@ public class LocalStack extends Stack {
         app.synth();
         System.out.println("App synthesizing in progress...");
     }
+
+
 }
